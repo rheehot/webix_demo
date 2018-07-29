e6a6709bcb29fdb5e63d3691e31057cf90d97271
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-07-29 18:59:34 +0900

    webix demo

---
+++ src/main/java/com/uvaper/webix/WebixApplication.java
@@ -0,0 +1,12 @@
+package com.uvaper.webix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebixApplication.class, args);
+    }
+}
